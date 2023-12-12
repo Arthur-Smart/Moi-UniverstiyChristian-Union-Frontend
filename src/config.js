af85cac6201a7cd7de4bbd9c61b72283c15bc5b0
unknown <mwangiarthur777@gmail.com>
2023-12-12 12:59:53 +0300

    new commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://moiuniversitycu.onrender.com/"
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://moiuniversitycu.onrender.com/",
 });
