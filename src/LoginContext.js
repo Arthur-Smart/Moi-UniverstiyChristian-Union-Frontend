00581e4a6610182e04d15e31c6412cbf806336d2
unknown <mwangiarthur777@gmail.com>
2023-12-12 12:36:09 +0300

    new commit

--- src/LoginContext.js
+++ src/LoginContext.js
@@ -1,34 +1,33 @@
 import { useState, createContext } from "react";
 
-const LoginContext = createContext()
+const LoginContext = createContext();
 
-const ContextProvider = ({children}) =>{
+const ContextProvider = ({ children }) => {
+  const admin = "exec@login.com";
+  const password = "exec12!pass591@@";
+  const user = JSON.parse(localStorage.getItem("user"));
 
-     const admin = 'exec@login.com'
-     const password = 'exec12!pass591@@'
-     const user = JSON.parse(localStorage.getItem('user'))
-
-    const LoginAdmin = ({adminName, adminPassword}) => {
-    if(adminName !== admin){
-      alert('Incorrect username')
-    } else if(adminPassword !== password ){
-      alert('Incorrect password')
-    }  else {
-      localStorage.setItem('user' , JSON.stringify({admin, password}))
-      window.location.replace('/admin')      
-    }   
-  }
+  const LoginAdmin = ({ adminName, adminPassword }) => {
+    if (adminName !== admin) {
+      alert("Incorrect username");
+    } else if (adminPassword !== password) {
+      alert("Incorrect password");
+    } else {
+      localStorage.setItem("user", JSON.stringify(true));
+      window.location.replace("/admin");
+    }
+  };
 
   const LogoutAdmin = () => {
-    localStorage.removeItem('user')
-    window.location.replace('/login')
-  }
+    localStorage.removeItem("user");
+    window.location.replace("/login");
+  };
 
   return (
-    <LoginContext.Provider value={{LoginAdmin, LogoutAdmin, user}}>
-        {children}
+    <LoginContext.Provider value={{ LoginAdmin, LogoutAdmin, user }}>
+      {children}
     </LoginContext.Provider>
-  )
-}
+  );
+};
 
-export {ContextProvider, LoginContext}
+export { ContextProvider, LoginContext };
